5722a8454de44a8ce78cae625119afe992020d30
José Marvin Cuellar Aguilar <cuellarmarvin14@gmail.com>
2024-11-12 12:48:49 -0600

    Cambiando intervalos denuevo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
             };
             console.log("Se ha subido")
             agregarMedicion(getDate(), getHour(), data)
-        }, 3000)  ; //1800000=30 minutos
+        }, 30000)  ; //1800000=30 minutos
 
 
         return () => clearInterval(interval);
