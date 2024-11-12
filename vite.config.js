a0e4a0fe7f4f2113663d3010b31a1d386f98bec7
Ernesto Alexander Aviles Moran <am18007@ues.edu.sv>
2024-11-11 20:43:05 -0600

    modificando vite

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     build: {
-      outDir: 'dist', // This ensures the output goes to 'dist'
-    },
-    server: {
-      host: true, // Esto permite que Vite esté accesible en la red local
-      port: 5173, // Puedes cambiarlo a otro puerto si el 5173 está ocupado
-    },
+      outDir: 'dist',
+    }
   });
